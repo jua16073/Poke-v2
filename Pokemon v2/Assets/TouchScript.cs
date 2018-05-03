5806549943a04c53e9bc66703cd1bdeaa9685035
Unknown <jua16073@uvg.edu.gt>
2018-05-02 18:59:01 -0600

    halp

---
+++ Pokemon v2/Assets/TouchScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Touch touch1 = Input.GetTouch(0);
+	}
+
+	void OnGUI(){
+		foreach(Touch touch in Input.touches){
+			string message= "";
+			message+= "ID: "+ touch.fingerId+ "\n";
+			message+= "Phase: "+touch.phase.ToString()+"\n";
+			message+= "TapCount: "+ touch.tapCount+"\n";
+			message+= "Pos X: "+ touch.position.x+"\n";
+			message+= "Pos Y: "+ touch.position.y+"\n";
+			int num= touch.fingerId;
+			GUI.Label(new Rect(0+130+num,0,120,100), message);
+		}
+	}
+}
